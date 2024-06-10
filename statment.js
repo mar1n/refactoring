f859583ff32ee17f4e91b90ed035ed771094d32e
mar1n <30738216+mar1n@users.noreply.github.com>
2024-06-10 15:33:55 +0100

    refactor code for book unknown change

--- statment.js
+++ statment.js
@@ -1,6 +1,6 @@
 function amountFor(aPerformance) {
   let result = 0;
-  switch (playFor(aPerformance).type) {
+  switch (aPerformance.play.type) {
     case "tragedy":
       result = 40000;
       if (aPerformance.audience > 30) {
@@ -15,24 +15,23 @@ function amountFor(aPerformance) {
       result += 300 * aPerformance.audience;
       break;
     default:
-      throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+      throw new Error(`unknown type: ${aPerformance.play.type}`);
   }
   return result;
 }
-
+const plays = {
+  hamlet: { name: "Hamlet", type: "tragedy" },
+  "as-like": { name: "As You Like It", type: "comedy" },
+  othello: { name: "Othello", type: "tragedy" },
+};
 function playFor(aPerformance) {
-  const plays = {
-    hamlet: { name: "Hamlet", type: "tragedy" },
-    "as-like": { name: "As You Like It", type: "comedy" },
-    othello: { name: "Othello", type: "tragedy" },
-  };
   return plays[aPerformance.playID];
 }
 
 function volumeCreditsFor(aPerformance) {
   let result = 0;
   result += Math.max(aPerformance.audience - 30, 0);
-  if ("comedy" === playFor(aPerformance).type)
+  if ("comedy" === aPerformance.play.type)
     result += Math.floor(aPerformance.audience / 5);
   return result;
 }
@@ -42,49 +41,84 @@ function usd(aNumber) {
     style: "currency",
     currency: "USD",
     minimumFractionDigits: 2,
-  }).format(aNumber);
+  }).format(aNumber / 100);
+}
+const invoice = {
+  customer: "BigCo",
+  performances: [
+    {
+      playID: "hamlet",
+      audience: 55,
+    },
+    {
+      playID: "as-like",
+      audience: 35,
+    },
+    {
+      playID: "othello",
+      audience: 40,
+    },
+  ].map(enrichPerformance),
+};
+function totalVolumeCredits(data) {
+  return data.reduce((total, p) => total + p.amount, 0);
 }
 
-function totalVolumeCredits(invoice) {
-  let volumeCredits = 0;
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-  }
-  return volumeCredits;
+function totalAmount(data) {
+  return data.reduce((total, p) => total + p.amount, 0);
 }
 
-function totalAmount(invoice) {
-  let result = 0;
+function statement(invoice, plays) {
+  let result = `Statment for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
-    result += amountFor(perf);
+    console.log("perf", perf);
+    console.log("playFor(perf).name", playFor(perf).name);
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
+      perf.audience
+    } seats)\n`;
   }
+
+  result += `Amount owed is ${usd(
+    totalAmount(invoice.performances)
+  )}\n`;
+  result += `You earned ${totalVolumeCredits(
+    invoice.performances
+  )} credits\n`;
   return result;
 }
 
-function statement(invoice) {
-    let result = `Statement for ${invoice.customer}\n`;
-    
-    for (let perf of invoice.performances) {
-        // print line for this order
-        result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
-            perf.audience
-        } seats)\n`;
-    }
+function enrichPerformance(aPerformance) {
+  const result = Object.assign({}, aPerformance);
+  result.play = playFor(result);
+  result.amount = amountFor(result);
+  result.volumeCredits = volumeCreditsFor(result);
+  return result;
+}
 
-  result += `Amount owed is ${usd(totalAmount(invoice) / 100)}\n`;
-  result += `You earned ${totalVolumeCredits(invoice)} credits\n`;
+function renderPlainText(data, invoice, plays) {
+  let result = `Statement for ${data.customer}\n`;
+
+  for (let perf of data.performances) {
+    // print line for this order
+    result += ` ${perf.play.name}: ${usd(perf.amount)} (${
+      perf.audience
+    } seats)\n`;
+  }
+
+  result += `Amount owed is ${usd(data.totalAmount)}\n`;
+  result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
 }
 
 async function szymon() {
   try {
-    const { default: [invoice] = json } = await import("./invoices.json", {
-      assert: { type: "json" },
-    });
+    // const { default: [invoice] = json } = await import("./invoices.json", {
+    //   assert: { type: "json" },
+    // });
 
-    const { default: plays } = await import("./plays.json", {
-      assert: { type: "json" },
-    });
+    // const { default: plays } = await import("./plays.json", {
+    //   assert: { type: "json" },
+    // });
     console.log(statement(invoice, plays));
   } catch (err) {
     console.log("err", err);
