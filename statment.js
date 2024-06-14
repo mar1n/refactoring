defcd0cffaec4ee10c308fc838ddc82b9f52aba8
mar1n <30738216+mar1n@users.noreply.github.com>
2024-06-14 17:11:26 +0100

    move create Statement data to separate file

--- statment.js
+++ statment.js
@@ -1,3 +1,4 @@
+import createStatementData from './createStatementData.js';
 import { writeFile, writeFileSync } from 'fs';
 
 function createFile(fileType, content) {
@@ -8,43 +9,6 @@ function createFile(fileType, content) {
         console.log('File has been created successfully!');
     }
 });
-}
-function amountFor(aPerformance) {
-  let result = 0;
-  switch (aPerformance.play.type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${aPerformance.play.type}`);
-  }
-  return result;
-}
-const plays = {
-  hamlet: { name: "Hamlet", type: "tragedy" },
-  "as-like": { name: "As You Like It", type: "comedy" },
-  othello: { name: "Othello", type: "tragedy" },
-};
-function playFor(aPerformance) {
-  return plays[aPerformance.playID];
-}
-
-function volumeCreditsFor(aPerformance) {
-  let result = 0;
-  result += Math.max(aPerformance.audience - 30, 0);
-  if ("comedy" === aPerformance.play.type)
-    result += Math.floor(aPerformance.audience / 5);
-  return result;
 }
 
 function usd(aNumber) {
@@ -69,47 +33,18 @@ const invoice = {
       playID: "othello",
       audience: 40,
     },
-  ].map(enrichPerformance),
+  ]
 };
-function totalVolumeCredits(data) {
-  return data.reduce((total, p) => total + p.amount, 0);
-}
-
-function totalAmount(data) {
-  return data.reduce((total, p) => total + p.amount, 0);
-}
 
 function statement(invoice, type) {
-  const statementData =  {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData.performances);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData.performances);
   return renderContent(createStatementData(invoice), type);
-}
-
-function createStatementData(invoice) {
-  const statementData =  {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData.performances);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData.performances);
-  return statementData
-}
-
-function enrichPerformance(aPerformance) {
-  const result = Object.assign({}, aPerformance);
-  result.play = playFor(result);
-  result.amount = amountFor(result);
-  result.volumeCredits = volumeCreditsFor(result);
-  return result;
 }
 
 function renderContent(data, type) {
   let newLine = type === "txt" ? `\n` : `</br>`;
   let result = `Statment for ${data.customer}${newLine}`;
   for (let perf of data.performances) {
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
+    result += ` ${perf.play.name}: ${usd(perf.amount)} (${
       perf.audience
     } seats)${newLine}`;
   }
